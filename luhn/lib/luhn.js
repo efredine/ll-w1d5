ced10158e8c7c580e57692ab850731eb0abadeb2
Eric Fredine <eric.fredine@gmail.com>
2016-11-04 20:13:14 +0000

    Added digit splitting function and test for it.

--- luhn/lib/luhn.js
+++ luhn/lib/luhn.js
@@ -1,5 +1,29 @@
+/*
+From wikipedia, the following implements the algorithm in python:
+def digits_of(number):
+    return [int(i) for i in str(number)]
+
+def luhn_checksum(card_number):
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    total = sum(odd_digits)
+    for digit in even_digits:
+        total += sum(digits_of(2 * digit))
+    return total % 10
+
+def is_luhn_valid(card_number):
+    return luhn_checksum(card_number) == 0
+
+*/
+
+function digits_of(number) {
+  return String(number).split("").map(Number);
+}
+
 function isValidLuhnCard(cardNumber) {
   return false;
 }
 
-module.exports = isValidLuhnCard;
+exports.isValidLuhnCard = isValidLuhnCard;
+exports.digits_of = digits_of;
--- luhn/test/luhn-test.js
+++ luhn/test/luhn-test.js
@@ -1,8 +1,15 @@
 var assert = require("chai").assert;
-var isValidLuhnCard = require("../lib/luhn");
+var luhn = require("../lib/luhn");
 
 describe("luhn", function() {
-  it("should return true for 79927398713", function() {
-    assert.isTrue(isValidLuhnCard(79927398713));
+  describe("digits_of", function() {
+    it("should turn 123 into [1,2,3]", function() {
+      assert.deepEqual(luhn.digits_of(123), [1,2,3]);
+    });
+  });
+  describe("isValidLuhnCard", function() {
+    it("should return true for 7992739L8713", function() {
+      assert.isTrue(isValidLuhnCard("79927398713".split("").map(Number)));
+    });
   });
 });
