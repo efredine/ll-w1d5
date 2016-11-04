3b290860a0e36c78425736ae18fc76c4db74ae58
Eric Fredine <eric.fredine@gmail.com>
2016-11-04 20:53:59 +0000

    Fix lint problems.

--- luhn/test/luhn-test.js
+++ luhn/test/luhn-test.js
@@ -4,13 +4,13 @@ var luhn = require("../lib/luhn");
 describe("luhn", function() {
   describe("digitsOf", function() {
     it("should turn 123 into [1,2,3]", function() {
-      assert.deepEqual(luhn.digitsOf(123), [1,2,3]);
+      assert.deepEqual(luhn.digitsOf(123), [1, 2, 3]);
     });
   });
   describe("stepDown", function() {
     it("should return a slice", function() {
-        assert.deepEqual(luhn.stepDown([1,2,3,4,5,6,7,8], -1, -2), [8,6,4,2]);
-        assert.deepEqual(luhn.stepDown([1,2,3,4,5,6,7,8], -2, -2), [7,5,3,1]);
+      assert.deepEqual(luhn.stepDown([1, 2, 3, 4, 5, 6, 7, 8], -1, -2), [8, 6, 4, 2]);
+      assert.deepEqual(luhn.stepDown([1, 2, 3, 4, 5, 6, 7, 8], -2, -2), [7, 5, 3, 1]);
     });
   });
   describe("isValidLuhnCard", function() {
